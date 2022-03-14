b9e207598e7ed9d30e3374e124c638055e473499
Tuba Adrienn <43565733+TubaAdrienn@users.noreply.github.com>
2022-03-14 10:25:28 +0100

    distance

--- mat_plot_main.py
+++ mat_plot_main.py
@@ -125,21 +125,34 @@ def draw_contour(im, conts, color):
     return im
 
 def draw_hull(im, conts, indexes, color):
+    points = []
     for i in range(len(indexes)-1):
         start_point= (conts[indexes[i]][1], conts[indexes[i]][0])
+        points.append(start_point)
         end_point=(conts[indexes[i+1]][1], conts[indexes[i+1]][0])
         im = cv2.line(im, start_point, end_point, color=color, thickness=2)
+    return im, points
+
+def distance(x1,y1, x2, y2, im):
+    distance = np.sqrt(np.power(x2-x1,2)+np.power(y2-y1,2))
+    if distance > 50:
+        im = cv2.circle(im, (x1,y1), radius=3, color=(255,0,255), thickness=-1)
     return im
 
 
-# Main
 
-im = cv2.imread("6.jpg")
+# Main
+im = cv2.imread("finger5.jpg")
 black_and_white = in_range_img(im)
 result, conts = find_contour(black_and_white)
 im = draw_contour(im, conts, (0, 0, 255))
 hull = find_convex_hull(conts)
-im = draw_hull(im, conts, hull, (0,255,0))
+im, points = draw_hull(im, conts, hull, (0,255,0))
+
+for i in range(len(points)-1):
+    im = distance(points[i][0], points[i][1], points[i+1][0], points[i+1][1], im)
+
+
 cv2.imshow("hull", im)
 cv2.waitKey(0)
 

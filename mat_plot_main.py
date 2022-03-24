b5e95f00c9c62beb771122a0ca9a4c59cbd3fc5f
Tuba Adrienn <43565733+TubaAdrienn@users.noreply.github.com>
2022-03-24 13:31:13 +0100

    lulz

--- mat_plot_main.py
+++ mat_plot_main.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 from statistics import mean
-import matplotlib.image as img
+import math
 
 def in_range_img(im):
     width, height = im.shape[:2]
@@ -49,7 +49,6 @@ def find_contour(im):
                 conts.append([i, j])
     return new_img, conts
 
-
 def left_index(points):
     minn = 0
     for i in range(1, len(points)):
@@ -87,37 +86,8 @@ def find_convex_hull(conts):
         p = q
         if (p == l):
             break
-
     return hull
 
-
-def find_defect_points(img, contours):
-    return True
-
-def count_fingers(defects, contours):
-    if defects is not None:
-        cnt = 0
-        biggie = False
-    for i in range(defects.shape[0]):  # calculate the angle
-
-        s, e, f = defects[i][0]
-        start = tuple(contours[s][0])
-        end = tuple(contours[e][0])
-        far = tuple(contours[f][0])
-
-        a = np.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
-        b = np.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
-        c = np.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
-        angle = np.arccos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  # cosine theorem
-        if angle <= np.pi / 2:  # angle less than 90 degree, treat as fingers
-            cnt += 1
-            cv2.circle(img, far, 4, [0, 255, 0], -1)
-        elif angle >= np.pi / 2 and angle <= np.pi / 1.5 and cnt == 0:
-            cnt = 1
-            biggie = True
-            cv2.circle(img, far, 4, [255, 0, 0], -1)
-    if cnt > 0 and biggie == False:
-        cnt = cnt + 1
 
 def draw_contour(im, conts, color):
     for i in range(len(conts)):
@@ -133,25 +103,32 @@ def draw_hull(im, conts, indexes, color):
         im = cv2.line(im, start_point, end_point, color=color, thickness=2)
     return im, points
 
-def distance(x1,y1, x2, y2, im):
-    distance = np.sqrt(np.power(x2-x1,2)+np.power(y2-y1,2))
-    if distance > 50:
-        im = cv2.circle(im, (x1,y1), radius=3, color=(255,0,255), thickness=-1)
-    return im
+def distance(points, im):
+    width, height = im.shape[:2]
+    points_result =[]
 
-
+    for i in range(len(points) - 1):
+        x1 = points[i][0]
+        y1= points[i][1]
+        x2= points[i+1][0]
+        y2 = points[i+1][1]
+        distance = np.sqrt(np.power(x2 - x1, 2) + np.power(y2 - y1, 2))
+        if distance > 50 and y1<height:
+            im = cv2.circle(im, (x1,y1), radius=3, color=(255,0,255), thickness=-1)
+            points_result.append((x1,y1))
+    return im, points_result
 
 # Main
-im = cv2.imread("finger5.jpg")
+im = cv2.imread("finger3.png")
 black_and_white = in_range_img(im)
 result, conts = find_contour(black_and_white)
 im = draw_contour(im, conts, (0, 0, 255))
 hull = find_convex_hull(conts)
 im, points = draw_hull(im, conts, hull, (0,255,0))
-
-for i in range(len(points)-1):
-    im = distance(points[i][0], points[i][1], points[i+1][0], points[i+1][1], im)
-
+im, points_result = distance(points,im)
+im = cv2.circle(im, (175,350), radius=3, color=(255, 0, 0), thickness=-1)
+for i in range(len(points_result)):
+    im = cv2.line(im, (175,350), points_result[i], color=(255,0,0), thickness=2)
 
 cv2.imshow("hull", im)
 cv2.waitKey(0)
